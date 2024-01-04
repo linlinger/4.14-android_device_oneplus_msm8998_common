c3a320305b13ac077722fd9bf20f3c0cf36fc323
Namkhai B <nk@nkeor.me>
2024-01-04 15:46:29 -0600

    msm8998-common: Rising bringup
    Signed-off-by: Namkhai B <nk@nkeor.me>

--- common.mk
+++ common.mk
@@ -490,3 +490,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/data/netmgr_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/netmgr_config.xml
 
 TARGET_SUPPORTS_QUICK_TAP := true
+
+# Rising flags
+RISING_CHIPSET := msm8998
+RISING_MAINTAINER := forkbomb9
+TARGET_BUILD_APERTURE_CAMERA := true
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BOOT_ANIMATION_RES := 1080
