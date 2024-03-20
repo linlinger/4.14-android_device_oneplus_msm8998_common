a5cb5f29d3c0559e9bd7f9efc38e1391727a92c9
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    msm8998-common: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- common.mk
+++ common.mk
@@ -11,9 +11,6 @@ ifeq ($(WITH_GMS), true)
     $(call inherit-product, vendor/oneplus/google/google.mk)
 endif
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
