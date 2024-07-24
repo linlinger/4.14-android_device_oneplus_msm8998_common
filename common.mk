02685f6844ab5d5e8d2b890c7c9b2497c3763ec6
Edrick Sinsuan <evcsinsuan@gmail.com>
2024-07-24 15:36:47 -0400

    msm8998-common: Build KeyHandler package
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- common.mk
+++ common.mk
@@ -271,6 +271,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# KeyHandler
+PRODUCT_PACKAGES += \
+    KeyHandler
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
